577a24d55854c1f735202c320ba0a94d699e6d90
Sanchit <you@example.com>
2020-05-15 22:36:36 +0530

    Deployment

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parents[1]
 SECRET_KEY = 'ul@6qn^%=n$he0%^!+(d+%lsmr@-+2)&1tzab%%%@x%ni!wu6^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['friendblogs.herokuapp.com']
 
