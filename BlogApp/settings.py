142d238bd5f91668c7938c945943e2295821229f
Sanchit <you@example.com>
2020-05-16 16:45:12 +0530

    Deploy

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'ul@6qn^%=n$he0%^!+(d+%lsmr@-+2)&1tzab%%%@x%ni!wu6^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['friendblogs.herokuapp.com']
 
 
 # Application definition
