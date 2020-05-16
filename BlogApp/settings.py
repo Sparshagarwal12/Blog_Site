e70ee1a44dfc5924cf5b486240820a0392dc0b2d
Sanchit <you@example.com>
2020-05-16 17:54:24 +0530

    kfndfjvn

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -129,12 +129,12 @@ DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'Blog/static'),
+    os.path.join(BASE_DIR, 'Blog/static'),
 )
 
 #  Add configuration for static files storage using whitenoise
